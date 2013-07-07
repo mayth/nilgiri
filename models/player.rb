ace40988c2cdf85350b720a392e000af2460f252
mayth <chimeaquas@hotmail.com>
2013-07-08 04:20:27 +0900

    Modify player model
    * Add twitter_id field
    * Set default value of registered_at

--- models/player.rb
+++ models/player.rb
@@ -4,6 +4,7 @@ class Player
   field :name, type: String
   field :screen_name, type: String
   field :pass, type: String
-  field :registered_at, type: DateTime
+  field :twitter_id, type: String
+  field :registered_at, type: DateTime, default: -> { Time.now }
   has_many :scores
 end
