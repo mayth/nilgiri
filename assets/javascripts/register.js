fc6530d72b1e1963eabc35c5e7932bda9dd52004
mayth <chimeaquas@hotmail.com>
2013-07-10 13:09:08 +0900

    Add register view

---
+++ assets/javascripts/register.js
@@ -0,0 +1,67 @@
+/***** register form *****/
+var machine_params = {
+  'iidx': {'type': 'int', 'diff': ['NORMAL', 'HYPER', 'ANOTHER']},
+  'popn': {'type': 'int', 'diff': ['NORMAL', 'HYPER', 'EX']},
+  'gf': {'type': 'perc', 'diff': ['GUITAR/BASIC', 'GUITAR/ADVANCED', 'GUITAR/EXTREME', 'GUITAR/MASTER', 'BASS/BASIC', 'BASS/ADVANCED', 'BASS/EXTREME', 'BASS/MASTER']},
+  'dm': {'type': 'perc', 'diff': ['BASIC', 'ADVANCED', 'EXTREME', 'MASTER']},
+  'ddr': {'type': 'int', 'diff': ['BEGINNER', 'BASIC', 'DIFFICULT', 'EXPERT', 'CHALLENGE']},
+  'jb': {'type': 'int', 'diff': ['BASIC', 'ADVANCED', 'EXTREME']},
+  'rb': {'type': 'perc', 'diff': ['BASIC', 'MEDIUM', 'HARD']},
+  'sdvx': {'type': 'int', 'diff': ['NOVICE', 'ADVANCED', 'EXHAUST', 'INFINITE']},
+  'dea': {'type': 'int', 'diff': ['LIGHT', 'STANDARD', 'EXTREME', 'MASTER', 'STEALTH']},
+  'ftt': {'type': 'int', 'diff': ['ベーシック', 'スーパー', 'ウルトラ']},
+  'mm': {'type': 'int', 'diff': ['EASY', 'BASIC', 'ADVANCED', 'EXPERT', 'MASTER']}
+};
+var type_patterns = {
+  'int': '(0|[1-9][0-9]*)',
+  'perc': '(0|[1-9][0-9]*)(\.[0-9]+)?'
+}
+
+function setDifficulty(diff_name) {
+  var $diff_sel = $('#difficulty');
+  $diff_sel.empty();
+  $.each(diff_name, function(idx, val) {
+    $diff_sel.append($('<option>').text(val));
+  });
+}
+
+function changeToIntegerForm($score_form) {
+  $score_form.attr('pattern', type_patterns['int']);
+  if ($score_form.parent().hasClass('input-append')) {
+    $score_form.unwrap();
+    $score_form.parent().children('.add-on').remove();
+  }
+}
+
+function changeToPercentForm($score_form) {
+  $score_form.attr('pattern', type_patterns['perc']);
+  $score_form.wrap($('<div>').addClass('input-append'));
+  $score_form.parent().append($('<span>').addClass('add-on').text('%'));
+}
+
+function setScoreType(type) {
+  var $score_form = $('#score');
+  if (type === 'int') {
+    changeToIntegerForm($score_form);
+  } else if (type === 'perc') {
+    changeToPercentForm($score_form);
+  } else {
+    changeToIntegerForm($score_form);
+  }
+}
+
+function setupForm() {
+  var v = machine_params[$('#machine option:selected').val()];
+  setDifficulty(v['diff']);
+  setScoreType(v['type']);
+}
+
+$(document).ready(function() {
+  // events
+  $('#machine').change(function() {
+    setupForm();
+  });
+
+  // init
+  setupForm();
+});
