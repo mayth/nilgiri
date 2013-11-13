bcec26986635e635bd911b24d1081b9ea1ffa070
mayth <chimeaquas@hotmail.com>
2013-11-13 19:55:09 +0900

    Add specs for Post model.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe Post do
+  describe 'create' do
+    before do
+      Post.create(
+        title: 'EXCELLENT!',
+        body: "You're excellent player!"
+      )
+      @post = Post.last
+    end
+    subject { @post }
+    context 'created successfully' do
+      it 'has correct attributes' do
+        expect(subject.title).to eq 'EXCELLENT!'
+        expect(subject.body).to eq "You're excellent player!"
+      end
+    end
+  end
 end
