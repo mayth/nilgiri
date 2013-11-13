ef08b21ad11db229bf58b20b99e672ed91c88b6f
mayth <chimeaquas@hotmail.com>
2013-11-13 19:46:14 +0900

    Add specs for Player model.

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe Player do
+  describe 'create' do
+    before do
+      Player.create(
+        name: 'maytheplic',
+        screen_name: 'Mei Akizuru',
+        password: 'HASHED_PASSWORD',
+        twitter_id: '@maytheplic'
+      )
+      @player = Player.find_by_name('maytheplic')
+    end
+    subject { @player }
+    context 'when successfully created' do
+      it 'has correct attributes' do
+        expect(subject.name).to eq 'maytheplic'
+        expect(subject.screen_name).to eq 'Mei Akizuru'
+        expect(subject.password).to eq 'HASHED_PASSWORD'
+        expect(subject.twitter_id).to eq '@maytheplic'
+      end
+    end
+  end
 end
