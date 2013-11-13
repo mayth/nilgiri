60a832766ea101332b6adcb038b8b2e172e1e472
mayth <chimeaquas@hotmail.com>
2013-11-13 19:46:00 +0900

    Add specs for Machine model.

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
 
 describe Machine do
+  describe 'create' do
+    before do
+      Machine.create(
+        name: 'sample',
+        difficulties: %w(BASIC MEDIUM HARD)
+      )
+      @machine = Machine.find_by_name('sample')
+    end
+    subject { @machine }
+    context 'when successfully created' do
+      it 'has correct name' do
+        expect(subject.name).to eq 'sample'
+      end
+      it 'has correct difficulties' do
+        subject.difficulties.should have(3).items
+        subject.difficulties[0].should eq('BASIC')
+        subject.difficulties[1].should eq('MEDIUM')
+        subject.difficulties[2].should eq('HARD')
+      end
+    end
+  end
 end
