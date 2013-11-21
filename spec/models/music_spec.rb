6af3f15d0bd0eb4315ec20b7a1dc06bda4511547
mayth <chimeaquas@hotmail.com>
2013-11-21 14:50:18 +0900

    Add Music spec

--- spec/models/music_spec.rb
+++ spec/models/music_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe Music do
+  describe 'create' do
+    before do
+      @machine = Machine.find_by_name('beatmaniaIIDX 21 SPADA')
+      Music.create(
+        name: 'Critical Crystal',
+        machine: @machine
+      )
+      @music = Music.find_by_name('Critical Crystal')
+    end
+    subject { @music }
+    context 'when successfully created' do
+      it 'has correct name' do
+        expect(subject.name).to eq 'Critical Crystal'
+      end
+      it 'has correct machine' do
+        expect(subject.machine).to eq @machine
+      end
+    end
+  end
 end
