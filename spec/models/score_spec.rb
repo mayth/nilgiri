796a7f4d33a5585769f7504954ae09c2bb9249b2
mayth <chimeaquas@hotmail.com>
2013-11-23 12:07:29 +0900

    Add Score spec

--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,4 +1,39 @@
 require 'spec_helper'
 
 describe Score do
+  describe 'create' do
+    before do
+      @machine = Machine.find_by_name('beatmaniaIIDX 21 SPADA')
+      @music = Music.create(
+        name: 'Round and Round',
+        machine: @machine
+      )
+      @player = Player.create(
+        name: 'maytheplic',
+        screen_name: 'Mei Akizuru',
+        password: 'PASSWORD',
+        twitter_id: '@maytheplic'
+      )
+      Score.create(
+        season: '201311',
+        difficulty: 'HYPER',
+        score: 1334.0,
+        player: @player,
+        music: @music,
+        playstyle: 'Single'
+      )
+      @score = @player.scores.where(season: '201311').first
+    end
+    subject { @score }
+    context 'when successfully created' do
+      it 'has correct attributes' do
+        expect(subject.season).to eq '201311'
+        expect(subject.difficulty).to eq 'HYPER'
+        expect(subject.score).to eq 1334
+        expect(subject.player).to eq @player
+        expect(subject.music).to eq @music
+        expect(subject.playstyle).to eq 'Single'
+      end
+    end
+  end
 end
