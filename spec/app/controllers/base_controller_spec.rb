fae183b070cbeb32f7506bcd157992a9966b70f0
mayth <chimeaquas@hotmail.com>
2013-11-26 00:47:45 +0900

    Remove spec

--- spec/app/controllers/base_controller_spec.rb
+++ spec/app/controllers/base_controller_spec.rb
@@ -4,8 +4,5 @@ describe "BaseController" do
   before do
     get "/"
   end
-
-  it "returns welcome message" do
-    expect(last_response.body).to include('Welcome to nilgiri')
-  end
+  # something
 end
