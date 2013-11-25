89005f943dd38ea008879aabb45404168be34f66
mayth <chimeaquas@hotmail.com>
2013-11-25 15:48:39 +0900

    Add Post#truncated_body

--- models/post.rb
+++ models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
-
+  def truncated_body(length: 30, omission: '...')
+    if body.length <= length
+      body
+    else
+      body[0..(length - omission.length - 1)] + omission
+    end
+  end
 end
