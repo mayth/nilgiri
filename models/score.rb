63532692c6c68f04eac223772977325da92341d6
mayth <chimeaquas@hotmail.com>
2013-11-25 17:00:28 +0900

    Add custom validator and fix specs for Score.

--- models/score.rb
+++ models/score.rb
@@ -1,4 +1,45 @@
+class ScoreValidator < ActiveModel::Validator
+  def validate(record)
+    # presence of season
+    unless record.season && !record.season.empty?
+      record.errors[:season] << "can't be empty."
+    end
+    # presence of music
+    unless record.music
+      record.errors[:music] << 'A score must be associated with a music.'
+    end
+    # presence of player
+    unless record.player
+      record.errors[:player] << 'A score must be associated with a player.'
+    end
+    # difficulty validation
+    unless record.difficulty && !record.difficulty.empty?
+      record.errors[:difficulty] << 'is required.'
+    else
+      if record.music && !record.music.machine.valid_difficulty?(record.difficulty)
+        record.errors[:difficulty] << 'is not valid.'
+      end
+    end
+    # playstyle validation
+    if record.music && record.music.machine && record.music.machine.playstyles
+      unless record.music.machine.valid_playstyle? record.playstyle
+        record.errors[:playstyle] << 'is not valid.'
+      end
+    end
+    # score validation
+    unless record.score
+      record.errors[:score] << "can't be empty."
+    else
+      unless record.score >= 0
+        record.errors[:score] << 'must be greater than or equal to 0.'
+      end
+    end
+  end
+end
+
 class Score < ActiveRecord::Base
   belongs_to :player
   belongs_to :music
+
+  validates_with ScoreValidator
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,28 +1,30 @@
 require 'spec_helper'
 
 describe Score do
+  before :all do
+    @machine = Machine.find_by_name('beatmania IIDX 21 SPADA')
+    @music = @machine.musics.create(
+      name: 'Round and Round',
+      artist: 'Masayoshi Minoshima'
+    )
+    @music.save!
+    @player = Player.create(
+      name: 'scorespec',
+      screen_name: 'ScoreSpec',
+      password: 'PASSWORD',
+      twitter_id: '@scorespec'
+    )
+    @player.save!
+    @player.register_score(
+      '201311',
+      @music,
+      'HYPER',
+      1334,
+      'SP'
+    ).save!
+  end
   describe 'create' do
     before do
-      @machine = Machine.find_by_name('beatmaniaIIDX 21 SPADA')
-      @music = Music.create(
-        name: 'Round and Round',
-        artist: 'Masayoshi Minoshima',
-        machine: @machine
-      )
-      @player = Player.create(
-        name: 'scorespec',
-        screen_name: 'ScoreSpec',
-        password: 'PASSWORD',
-        twitter_id: '@scorespec'
-      )
-      Score.create(
-        season: '201311',
-        difficulty: 'HYPER',
-        score: 1334.0,
-        player: @player,
-        music: @music,
-        playstyle: 'Single'
-      )
       @score = @player.scores.where(season: '201311').first
     end
     subject { @score }
@@ -33,7 +35,54 @@ describe Score do
         expect(subject.score).to eq 1334
         expect(subject.player).to eq @player
         expect(subject.music).to eq @music
-        expect(subject.playstyle).to eq 'Single'
+        expect(subject.playstyle).to eq 'SP'
+      end
+    end
+  end
+  describe '::new' do
+    context 'when validation fails' do
+      it 'is not saved' do
+        expect(Score.new(season: nil).save).to be_false
+        expect(Score.new(
+          season: '201311',
+          difficulty: 'BASIC', # invalid
+          score: 1334,
+          player: @player,
+          music: @music,
+          playstyle: 'SP'
+        ).save).to be_false
+        expect(Score.new(
+          season: '201311',
+          difficulty: 'HYPER',
+          score: -10, # invalid
+          player: @player,
+          music: @music,
+          playstyle: 'SP'
+        ).save).to be_false
+        expect(Score.new(
+          season: '201311',
+          difficulty: 'HYPER',
+          score: 334,
+          player: nil,
+          music: @music,
+          playstyle: 'SP'
+        ).save).to be_false
+        expect(Score.new(
+          season: '201311',
+          difficulty: 'HYPER',
+          score: 334,
+          player: @player,
+          music: nil,
+          playstyle: 'SP'
+        ).save).to be_false
+        expect(Score.new(
+          season: '201311',
+          difficulty: 'HYPER',
+          score: 334,
+          player: @player,
+          music: @music,
+          playstyle: 'GUITAR'
+        ).save).to be_false
       end
     end
   end
