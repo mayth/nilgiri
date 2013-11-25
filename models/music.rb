0f44a71f41680c5cae4a242c3acb90cd53739895
mayth <chimeaquas@hotmail.com>
2013-11-25 15:25:15 +0900

    Add validations for Music.

--- models/music.rb
+++ models/music.rb
@@ -1,4 +1,8 @@
 class Music < ActiveRecord::Base
   has_many :scores
   belongs_to :machine
+
+  validates :name, presence: true
+  validates :artist, presence: true
+  validates_associated :machine
 end
