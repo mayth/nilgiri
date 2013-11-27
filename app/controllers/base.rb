5e877715bf2111dd689e22150fb2851272bdcab8
mayth <chimeaquas@hotmail.com>
2013-11-27 17:05:34 +0900

    Implement /index view with Foundation.

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -1,5 +1,10 @@
 Nilgiri::App.controllers :base do
   get :index, :map => '/' do
+    @machines = Machine.all
+    @top_post = Post.first
+    @posts = Post.offset(1).limit(5)
+    @is_more_posts = Post.count > 6
+    @current_season = '201311'
     render 'base/index'
   end
 end
