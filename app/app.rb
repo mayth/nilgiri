d7a03d6cf81d04496d2e6f58660c080a849e84c3
mayth <chimeaquas@hotmail.com>
2013-11-27 19:12:29 +0900

    Use sass compiler only if the environment is not production.

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,6 @@
 module Nilgiri
   class App < Padrino::Application
-    register SassInitializer
+    register SassInitializer unless Padrino.env == :production
     use ActiveRecord::ConnectionAdapters::ConnectionManagement
     register Padrino::Rendering
     register Padrino::Mailer
