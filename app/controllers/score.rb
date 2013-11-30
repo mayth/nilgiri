c0182493c5aae589fe1d7a70c9b6ae2ec1f5fbc8
mayth <chimeaquas@hotmail.com>
2013-12-01 05:06:45 +0900

    Add score registration form.

--- app/controllers/score.rb
+++ app/controllers/score.rb
@@ -1,16 +1,46 @@
 Nilgiri::App.controllers :score do
+  before do
+    @page_id = 'score'
+    @current_season = Time.now.strftime('%Y%m')
+  end
+
   get :index do
     @recent = Score.order(updated_at: :desc).limit(20)
     render 'score/index'
   end
 
   get :new do
-    @machines = Machine.all.map {|m| [m.name, m.slug]}
-    @current_season = Time.now.strftime('%Y%m')
+    @score = Score.new
+    @machines = Machine.all.map {|m| [m.name, m.id]}
     render 'score/new'
   end
 
   post :create do
+    @machines = Machine.all.map {|m| [m.name, m.id]}
+
+    score = params[:score]
+    # Authorize player
+    player_name, password =
+      score.delete('player_name'), score.delete('password')
+    begin
+      player = Player.authorize(player_name, password)
+    rescue ActiveRecord::RecordNotFound
+      flash.now[:warning] = "The player was not found."
+      render 'score/new'
+    end
+    score['player'] = player
+    # Remove machine ID
+    score.delete('machine_id')
+
+    # Save score
+    @score = Score.new(params[:score])
+    if @score.save
+      flash[:info] = "Your score has been successfully submitted."
+      redirect url(:score, :index)
+    else
+      flash.now[:warning] = "Failed to save your score."
+      render 'score/new'
+    end
   end
 
 end
