9ae25ca64fa8570705d0d2abbfe52f5c618a39f6
mayth <chimeaquas@hotmail.com>
2013-12-01 14:37:42 +0900

    Add Season#range

--- models/season.rb
+++ models/season.rb
@@ -1,3 +1,5 @@
 class Season < ActiveRecord::Base
-
+  def range
+    (start..expiry)
+  end
 end
--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe Season do
+  describe '#range' do
+    before do
+      @now = Time.local(2013, 12, 1, 9, 0, 0, 0)  # 2013-12-01 09:00:00
+      Season.create(
+        name: 'season testing 1',
+        start: @now,
+        expiry: @now + (60 * 60 * 24)  # + 1 day
+      )
+      @season = Season.find_by_name('season testing 1')
+    end
+    subject { @season.range }
+    it 'returns Range' do
+      expect(subject).to be_kind_of Range
+    end
+    it 'returns correct range' do
+      expect(subject.begin).to eql @now
+      expect(subject.last).to eql (@now + (60 * 60 * 24))
+    end
+  end
 end
