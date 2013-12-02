49c31ede5d946a88d4f37f8d0e4450adcb1857d4
mayth <chimeaquas@hotmail.com>
2013-12-02 22:36:13 +0900

    Remove editing/deleting a player features.

--- app/controllers/player.rb
+++ app/controllers/player.rb
@@ -23,52 +23,10 @@ Nilgiri::App.controllers :player do
     @player = Player.new(params[:player])
     if @player.save
       flash[:success] = "Your account \"#{@player.name}\" has been successfully created."
-      redirect(url(:player, :edit, id: @player.id))
+      redirect(url(:player))
     else
       flash.now[:warning] = "Failed to create your account!"
       render 'player/new'
-    end
-  end
-
-  get :edit, with: :id do
-    begin
-      @player = Player.find(params[:id])
-      render 'player/edit'
-    rescue ActiveRecord::RecordNotFound
-      flash.now[:alert] = "Player #{params[:id]} was not found."
-      halt 404
-    end
-  end
-
-  put :update, with: :id do
-    begin
-      @player = Player.find(params[:id])
-      if @player.update_attributes(params[:player])
-        redirect(url(:player, :edit, id: @player.id))
-      else
-        flash.now[:alert] = "Failed to update the account."
-        render 'player/edit'
-      end
-    rescue ActiveRecord::RecordNotFound
-      flash.now[:alert] = "Player #{params[:id]} was not found."
-      halt 404
-    end
-  end
-
-  delete :destroy, with: :id do
-    begin
-      player = Player.find(params[:id])
-      if player.destroy
-      else
-        flash.now[:alert] = "Failed to delete the account."
-      end
-      redirect url(:player, :index)
-    rescue ActiveRecord::RecordNotFound
-      flash.now[:alert] = "Player #{params[:id]} was not found."
-      halt 404
-    else
-      flash.now[:alert] = 'Something went wrong.'
-      halt 500
     end
   end
 end
