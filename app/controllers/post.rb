31631fafe064af5e6bcea786bb5a6e470848289b
mayth <chimeaquas@hotmail.com>
2013-12-03 18:46:42 +0900

    Add the index page for posts.

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -1,30 +1,10 @@
 Nilgiri::App.controllers :post do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
   get :index do
-
+    @posts = Post.all
+    render 'post/index'
   end
 
-  get :show do
-
+  get :show, with: :id do
   end
 
 end
