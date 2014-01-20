e5b41eadb752cba6a666860fe54c744bf28b2954
mayth <chimeaquas@hotmail.com>
2014-01-21 07:54:43 +0900

    Add validations for Post.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.create' do
+    context 'when valid input is given' do
+      it 'should be saved' do
+        expect(build(:post).save).to be_true
+      end
+    end
+    context 'when invalid input is given' do
+      it 'should not be saved if title is empty' do
+        expect(build(:post, title: '').save).to be_false
+      end
+      it 'should not be saved if body is empty' do
+        expect(build(:post, body: '').save).to be_false
+      end
+    end
+  end
 end
