2ab982a86cc618d19986b319af52380f7c58730f
mayth <chimeaquas@hotmail.com>
2014-01-21 07:49:37 +0900

    Scaffolding Post model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Nilgiri::Application.routes.draw do
+  resources :posts
+
   resources :players
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140120214302) do
+ActiveRecord::Schema.define(version: 20140120224907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,5 +26,12 @@ ActiveRecord::Schema.define(version: 20140120214302) do
   end
 
   add_index "players", ["name"], name: "index_players_on_name", unique: true, using: :btree
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
