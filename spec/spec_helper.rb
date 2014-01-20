744140d5e2ae679d689307d3e9e14817fe1b64b6
mayth <chimeaquas@hotmail.com>
2014-01-21 06:05:19 +0900

    Ready for testing.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,32 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'factory_girl_rails'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = true
+  config.order = "random"
+
+  config.before :suite do
+    DatabaseCleaner.clean_all
+  end
+
+  config.after :each do
+    DatabaseCleaner.clean
+  end
+
+  config.before :all do
+    FactoryGirl.reload
+  end
+
+  config.include FactoryGirl::Syntax::Methods
+end
