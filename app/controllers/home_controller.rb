92844c8ec3859ac10562b271d64c063f143f690d
mayth <chimeaquas@hotmail.com>
2014-02-10 03:13:25 +0900

    Implement ranking in Home#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+    @posts = Post.order(updated_at: :desc)
+    n = @posts.count
+    @top_post = @posts.first
+    if n >= 2
+      @recent_posts = @posts.offset(1).limit(5)
+      @is_more_posts = n > 6
+    end
+
+    if @current_season.present?
+      @top_scores = Hash[Machine.all.map {|machine|
+        [machine, machine.get_top_scores(@current_season)]
+      }]
+    end
   end
 end
